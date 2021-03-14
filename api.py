7ea21693cc0840a11df5e328ac25aca0c84a44a1
rasmus h <cph-rh178@cphbusiness.dk>
2021-03-14 20:01:10 +0100

    db setup

--- api.py
+++ api.py
@@ -1,13 +1,26 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy 
+from flaskext.mysql import MySQL
+import yaml
 #from flask_restful import Resource, Api, reqparse
 #import pandas as pd
 #import ast
 
 
 app = Flask(__name__)
-app.configi['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///funkdb.db'
-db = SQLAlchemy(app)
+
+#db config
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
+
+
+#db = SQLAlchemy(app)
 #api = Api(app)
 
 
