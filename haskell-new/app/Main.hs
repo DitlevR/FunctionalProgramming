8d684e48715e7950aa361e8696c0c7bcbf663ba3
DitlevR <ditlev1997@hotmail.com>
2021-04-09 13:20:07 +0200

    a

--- haskell-new/app/Main.hs
+++ haskell-new/app/Main.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE BlockArguments #-}
 module Main where
 
 import Lib
+import Web.Scotty
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
+--import qualified Data.Text.Lazy as L
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Starting server at 4711"
+    scotty 4711 $ do --scottyM monad
+         get "/em1" $ do
+            json em1
+
+
+
+em1 :: Employee
+em1 = Employee 1 "John" "John@johnmail.com"
+
+
+data Employee = Employee
+    { id :: Integer
+    , name :: String
+    , email :: String
+    } deriving (Show, Generic)
+
+instance ToJSON Employee
+
