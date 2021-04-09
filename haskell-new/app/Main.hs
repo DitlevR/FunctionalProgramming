3cb51a8adad05823b7c1728dd6e893f2b46f70e2
rasmus h <cph-rh178@cphbusiness.dk>
2021-04-09 13:54:22 +0200

    a

--- haskell-new/app/Main.hs
+++ haskell-new/app/Main.hs
@@ -4,28 +4,7 @@
 module Main where
 
 import Web.Scotty
-import Database.MySQL.Base
-import qualified System.IO.Streams as Streams
 
-
-main :: IO ()
-main = do
-    putStrLn("Starting server at 4711....")
-    scotty 4711 (do --ScottyM Monad
-        get "/hello" (do --ActionM Monad
-             text "Hello World!"
-            )
-
-        )
-
-getData ::  =
-               conn <- connect
-                   defaultConnectInfo {ciUser = "username", ciPassword = "password", ciDatabase = "dbname"}
-               (defs, is) <- query_ conn "SELECT * FROM some_table"
-               print =<< Streams.toList ismodule Main where
-
-import Lib
-import Web.Scotty
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
 --import qualified Data.Text.Lazy as L
